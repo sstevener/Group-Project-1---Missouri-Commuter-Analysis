66dacaf440b00a6695ee700f1ca4dc6e619eec02
Scott Stevener <43261873+sstevener@users.noreply.github.com>
2019-01-24 00:09:18 -0600

    Initial Upload

---
+++ Data Analysis/config.py
@@ -0,0 +1,4 @@
+#config.py
+
+gkey = "AIzaSyCmip0faMZ-KYZRBNhZed47vZb__CwYExM"
+api_key = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
---
+++ Data Analysis/sample_config.py
@@ -0,0 +1,4 @@
+api_key = < Your Census API Key Here >
+gkey = < Your Google Maps API Key Here >
+
+#real config file should be named "config.py"
